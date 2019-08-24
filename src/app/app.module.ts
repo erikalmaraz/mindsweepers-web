82cc18893741c46b4764d5646d27a8b93abe6971
Erik Almaraz <erkalmar@gmail.com>
2019-08-24 18:19:45 -0500

    Add assets - Add components of the first mock design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,

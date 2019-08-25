55ff56b4296035779a1d0ef0f649e99be431dde9
Erik Almaraz <erkalmar@gmail.com>
2019-08-25 10:33:24 -0500

    Add core methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Views & Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
@@ -9,6 +9,8 @@ import { LoginComponent } from './views/login/login.component';
 import { HomeComponent } from './views/home/home.component';
 import { NotificationsComponent } from './views/notifications/notifications.component';
 import { WelcomeComponent } from './views/welcome/welcome.component';
+// Services
+import { ProductsService } from './core/services/products.service';
 
 @NgModule({
   declarations: [
